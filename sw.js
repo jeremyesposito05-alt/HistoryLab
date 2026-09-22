3a9dff1918c259d01b130a10cbb9cfe58fb69e4b
jeremyesposito05-alt <jeremy.esposito.05@gmail.com>
2026-09-22 15:51:05 +0200

    HistoryLab V7.6: refresh cache after revision navigation fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'historylab-v7.5-pedagogy-priority1';
+const CACHE = 'historylab-v7.6-revision-navigation-fix';
 const CORE = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
