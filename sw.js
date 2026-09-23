7056737e102c0da1ce6be6a10a59725b1673769c
jeremyesposito05-alt <jeremy.esposito.05@gmail.com>
2026-09-23 07:10:21 +0200

    HistoryLab V7.11: refresh cache after profile and Flash fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'historylab-v7.10-daily-queue';
+const CACHE = 'historylab-v7.11-profile-flash-fix';
 const CORE = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
